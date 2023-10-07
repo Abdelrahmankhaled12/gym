459406c369c8d50d5c7b9f25799042a23dffdc43
Abdelrahmankhaled12 <abdelrahman.khaled1086@gmail.com>
2023-10-07 14:02:13 +0300

    up

--- src/sections/chooseWork/ChooseWorkOut.jsx
+++ src/sections/chooseWork/ChooseWorkOut.jsx
@@ -1,11 +1,9 @@
 import "./style.scss"
 import ContentWrapper from "../../components/contentWrapper/ContentWrapper"
-import Select from "react-select";
 import { useState, useEffect } from "react";
-import Spinner from "../../components/spinner/Spinner";
-import { goal, level, tags, results } from "./data";
+import { tags, results } from "./data";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faHandPointDown , faHandBackFist } from '@fortawesome/free-solid-svg-icons'
+import { faHandBackFist } from '@fortawesome/free-solid-svg-icons'
 import Aos from "aos";
 import "aos/dist/aos.css"
 
@@ -18,64 +16,18 @@ const ChooseWorkOut = () => {
     }, [])
 
     const [steps, setSteps] = useState(0);
-    const [goalValue, setGoalValue] = useState(null);
-    const [levelValue, setLevelValue] = useState(null);
-    const [tagsValue, setTagsValue] = useState(null);
-    const [finish, setFinish] = useState(false);
-    const [showSpinner, setShowSpinner] = useState(true);
-    const [chooseGoal, setChooseGoal] = useState(false);
-    const [chooseLevel, setChooseLevel] = useState(false);
     const [chooseTags, setChooseTags] = useState(false);
-    const [showPlan, setShowPlan] = useState(false);
     const [result, setResult] = useState("");
 
 
-    const onChange = (selectedItems, action) => {
-        if (action.name === "goal") {
-            setGoalValue(selectedItems.value)
-            if (!chooseGoal) setSteps((prev) => prev + 1);
-            setChooseGoal(true);
-        } else if (action.name === "level") {
-            setLevelValue(selectedItems.value)
-            if (!chooseLevel) setSteps((prev) => prev + 1);
-            setChooseLevel(true);
-        } else {
-            setTagsValue(selectedItems.value)
-            if (!chooseTags) setSteps((prev) => prev + 1);
-            setChooseTags(true);
-        }
-    }
-
-    const finishClick = () => {
-        if (finish) {
-            setSteps(0);
-            setShowPlan(false);
-            setChooseGoal(false);
-            setChooseLevel(false);
-            setChooseTags(false);
-            setResult("");
-            setGoalValue(null)
-            setLevelValue(null)
-            setTagsValue(null)
-        } else {
-            results.forEach(element => {
-                console.log(element.values.join("|"));
-                console.log(goalValue + "|" + levelValue + "|" + tagsValue);
-                if (element.values.join("|") === (goalValue + "|" + levelValue + "|" + tagsValue)) {
-                    setResult(element.result)
-                }
-
-            })
-        }
-
-        if (steps === 3 && !finish) {
-            setShowSpinner(true);
-            setTimeout(() => {
-                setShowSpinner(false);
-                setShowPlan(true);
-            }, 3000);
-        }
-        setFinish(!finish);
+    const onChange = (event) => {
+        if (!chooseTags) setSteps((prev) => prev + 1);
+        setChooseTags(true);
+        results.forEach(element => {
+            if (element.value === event.target.value) {
+                setResult(element.result)
+            }
+        })
     }
 
     return (
@@ -84,52 +36,14 @@ const ChooseWorkOut = () => {
                 <div data-aos="fade-up" data-aos-delay="400" data-aos-duration="1000">
                     <h1>Choose Workout <FontAwesomeIcon icon={faHandBackFist} /></h1>
                     <div className="filters">
-                        <Select
-                            name="goal"
-                            options={goal}
-                            onChange={onChange}
-                            placeholder={goalValue === null ? "Goal" : goalValue}
-                            className="react-select-container genresDD genresDD-1"
-                            classNamePrefix="react-select"
-                            value={goalValue}
-                        />
-                        <Select
-                            name="level"
-                            options={level}
-                            onChange={onChange}
-                            placeholder={levelValue === null ? "Level" : levelValue}
-                            className="react-select-container genresDD genresDD-2"
-                            classNamePrefix="react-select"
-                            value={levelValue}
-                        />
-                        <Select
-                            name="frequency"
-                            options={tags}
-                            onChange={onChange}
-                            placeholder={tagsValue === null ? "Tags" : tagsValue}
-                            className="react-select-container genresDD genresDD-3"
-                            classNamePrefix="react-select"
-                            value={tagsValue}
-                        />
+                        {tags.map((element, index) => (
+                            <div className="checkBox" key={index}>
+                                <input type="radio" name="work" id={element} onChange={onChange} value={element} />
+                                <label htmlFor={element}>{element}</label>
+                            </div>
+                        ))}
                     </div>
-                    <div className="progress">
-                        <p className="void"></p>
-                        <p className="busy" style={{ width: steps * 33.333333333 + "%" }}></p>
-                    </div>
-                    <button className={`${steps === 3 ? "" : "hide"}`} onClick={finishClick}>{finish ? "Reset" : "Finish"}</button>
-                    {finish &&
-                        <>
-                            {showSpinner && <Spinner />}
-                            {showPlan && (
-                                <>
-                                    <div className="icon">
-                                        <FontAwesomeIcon icon={faHandPointDown} />
-                                    </div>
-                                    <a href={result}>Go to training</a>
-                                </>
-                            )}
-                        </>
-                    }
+                    <a href={result} className={`${steps === 1 ? "" : "hide"}`}>Go to Workout</a>
                 </div>
             </ContentWrapper>
         </div>
--- src/sections/chooseWork/data.js
+++ src/sections/chooseWork/data.js
@@ -1,32 +1,55 @@
-export let goal= [
-    { value: "Lose weight", label: "Lose weight" },
-    { value: "Gain muscle", label: "Gain muscle" },
-    { value: "Flexibility and mobility", label: "Flexibility and mobility" }
-]
-
-export let level = [
-    { value: "Beginner", label: "Beginner" },
-    { value: "Intermediate", label: "Intermediate" },
-    { value: "Advanced", label: "Advanced" }
-]
-
 export let tags = [
-    { value: "functional", label: "functional" },
-    { value: "Metabolic", label: "Metabolic" },
-    { value: "HIIT", label: "HIIT" }
+    "Tabata/w001",
+    "Tabata/w002",
+    "EMOM/w003",
+    "EMOM/w004",
+    "Metabolic/HIIT/w005",
+    "Metabolic/HIIT/w006",
+    "Functional/w007",
+    "Functional/w008",
+    "Circuit/w009",
+    "Circuit/w010"
 ]
 
 export let results = [
     {
-        values:["Lose weight","Beginner","functional"],
-        result:"https://f.io/071DhRtU"
-    }
+        value: "Tabata/w001" ,
+        result: "https://app.frame.io/presentations/d8cbe31a-930e-488e-a00a-9cfcba26d13e"
+    },
+    {
+        value: "Tabata/w002",
+        result:"https://app.frame.io/presentations/8aef90df-80f1-456b-9992-a71ef5fc8c71"
+    },
+    {
+        value: "EMOM/w003" ,
+        result:"https://app.frame.io/presentations/02cc059c-8f76-45e9-bde6-0999d52cd092"
+    },
+    {
+        value: "EMOM/w004",
+        result:"https://app.frame.io/presentations/a36a4d20-9d41-4d99-97ce-2125db13e066"
+    },
+    {
+        value: "Metabolic/HIIT/w005",
+        result:"https://app.frame.io/presentations/3168d0b1-e4ef-4587-a5ac-820d05b76ac0"
+    },
+    {
+        value: "Metabolic/HIIT/w006",
+        result:"https://app.frame.io/presentations/6024cd4c-99e0-43ac-a564-60890d4e126e"
+    },
+    {
+        value: "Functional/w007" ,
+        result:"https://app.frame.io/presentations/fd0abb45-b519-4a14-a2d2-1099a3d03a15"
+    },
+    {
+        value: "Functional/w008" ,
+        result:"https://app.frame.io/presentations/b0d1fc2d-79e8-4e09-adc3-eb9972810891"
+    },
+    {
+        value: "Circuit/w009" ,
+        result:"https://app.frame.io/presentations/f9746495-e883-49be-ac66-413350f01bfd"
+    },
+    {
+        value: "Circuit/w010" ,
+        result:"https://app.frame.io/presentations/9b8827b1-753a-450a-bfe9-290dd49e53c7"
+    },
 ]
-
-
-
-// export let goal_plan = [
-//     { value: "", label: "" },
-//     { value: "", label: "" },
-//     { value: "", label: "" }
-// ]
